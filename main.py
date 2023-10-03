41a0c5c45535002d44ac3a8c7f3d03e2dfaa155b
Jim Hessin <jhessin@gmail.com>
2023-10-02 19:14:09 -0600

    pyqtgraph example

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
+from pyqtgraph import PlotWidget
+
+
+class MainWindow(QMainWindow):
+    graphWidget: PlotWidget
+
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('MainWindow.ui', self)
+
+        self.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [30, 32, 34, 32, 33, 31, 29, 32, 35, 45])
+
+    def plot(self, hour, temperature):
+        self.graphWidget.plot(hour, temperature)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = uic.loadUi("MainWindow.ui")
+    window = MainWindow()
     window.show()
     # ...
     sys.exit(app.exec())
