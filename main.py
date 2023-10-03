612bebdb8f598089ff7a469f7141dca8809c7f94
Jim Hessin <jhessin@gmail.com>
2023-10-03 17:37:55 -0600

    list view

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 # This Python file uses the following encoding: utf-8
+import os
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+
 from PyQt6 import uic
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow,
+    QPushButton, QListWidget, QListWidgetItem, QMenuBar, QStatusBar,
+)
+from PyQt6.QtCore import QModelIndex
 
 
 class MainWindow(QMainWindow):
+    pullAllButton: QPushButton
+    pullRepoButton: QPushButton
+    pushAllButton: QPushButton
+    pushRepoButton: QPushButton
+
+    addRepo: QPushButton
+    rmRepo: QPushButton
+
+    repoList: QListWidget
+    searchButton: QPushButton
+
+    menubar: QMenuBar
+    statusbar: QStatusBar
 
     def __init__(self):
         super().__init__()
 
         uic.loadUi('MainWindow.ui', self)
+
+        self.searchButton.clicked.connect(self.search_for_repos)
+        self.repoList.itemClicked.connect(self.repo_selected)
+
+    def search_for_repos(self):
+        values = ['one', 'two', 'three']
+        for i in values:
+            self.repoList.addItem(QListWidgetItem(i))
+
+    def repo_selected(self, item: QListWidgetItem):
+        print(item.text())
 
 
 if __name__ == "__main__":
