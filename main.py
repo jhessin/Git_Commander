468fc579da04d7c9e4434b8b8be32f7c1c21b4ae
Jim Hessin <jhessin@gmail.com>
2023-10-07 21:50:52 -0600

    "2023-10-07 21:50:52.542586"

--- main.py
+++ main.py
@@ -68,15 +68,15 @@ def push_repo(path: str):
     print(f"committing and pushing {path}")
     # TODO: commit all and push the repo
     date = datetime.now()
-    subprocess.run('git add .', cwd=path)
-    subprocess.run(f"git commit -m \"{date}\"", cwd=path)
-    subprocess.run('git push', cwd=path)
+    subprocess.run(['git',  'add', '.'], cwd=path)
+    subprocess.run(['git', 'commit',  f'-m "{date}"'], cwd=path)
+    subprocess.run(['git', 'push'], cwd=path)
 
 
 def pull_repo(path: str):
     print(f"pulling {path}")
     # TODO: pull the given repo
-    subprocess.run('git pull', cwd=path)
+    subprocess.run(['git', 'pull'], cwd=path)
 
 
 class RepoSearch(QRunnable):
