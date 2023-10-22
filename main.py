9ae987442f3241beb1a57db97611449b9e7a3657
Jim Hessin <jhessin@gmail.com>
2023-10-21 19:12:07 -0600

    Making pylint happy.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # This Python file uses the following encoding: utf-8
-import logging
+"""
+Git Commander
+
+GitCommander is a basic gui for handling multiple git repositories.
+"""
 import os
 import pickle
 import subprocess
@@ -11,7 +15,10 @@ from PyQt6 import uic
 from PyQt6.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot, QThreadPool, Qt
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow,
-    QPushButton, QListWidget, QMenuBar, QStatusBar, QPlainTextEdit, QFileDialog, QDialog, QDialogButtonBox,
+    QPushButton, QListWidget,
+    QMenuBar, QStatusBar,
+    QFileDialog,
+    QDialog, QDialogButtonBox,
     QListWidgetItem,
 )
 
@@ -28,7 +35,9 @@ def data_path(*relative_path: str) -> os.path:
     :param relative_path: The relative path of the data object you want
     :return: The absolute path within the config directory
     """
-    base_path = getattr(sys, 'PKG_CONFIG_PATH', os.path.join(os.path.expanduser('~'), '.config', 'GitCommander'))
+    base_path = getattr(sys, 'PKG_CONFIG_PATH',
+                        os.path.join(os.path.expanduser('~'), '.config'))
+    base_path = os.path.join(base_path, 'GitCommander')
     os.makedirs(base_path, exist_ok=True)
     return os.path.join(base_path, *relative_path)
 
@@ -37,22 +46,11 @@ PICKLE_FILE = data_path('repos.dat')
 REPOS_DIRECTORY = os.path.join(os.path.expanduser('~'), 'repos')
 
 
-class QTextEditLogger(logging.Handler, QObject):
-    appendPlainText = pyqtSignal(str)
-
-    def __init__(self, parent):
-        super().__init__()
-        QObject.__init__(self)
-        self.widget = QPlainTextEdit(parent)
-        self.widget.setReadOnly(True)
-        self.appendPlainText.connect(self.widget.appendPlainText)
-
-    def emit(self, record):
-        msg = self.format(record)
-        self.appendPlainText.emit(msg)
-
-
 def load_repos() -> list[str]:
+    """
+    Load the repos from the pickle file.
+    :return: a list of directories that hold repos.
+    """
     if os.path.exists(PICKLE_FILE):
         with open(PICKLE_FILE, 'rb') as file:
             return load(file)
@@ -61,6 +59,11 @@ def load_repos() -> list[str]:
 
 
 def list_repos(data: QListWidget) -> list[str]:
+    """
+    Get the list of repos from the provided QListWidget.
+    :param data: A QListWidget that holds strings.
+    :return: a list of strings
+    """
     items = []
     for i in range(data.count()):
         items.append(data.item(i).text())
@@ -68,35 +71,51 @@ def list_repos(data: QListWidget) -> list[str]:
 
 
 def save_repos(data: QListWidget):
+    """
+    Save the repos from a QListWidget
+    :param data: The QListWidget to get data from
+    :return: None
+    """
     items = list_repos(data)
-    try:
-        with open(PICKLE_FILE, 'wb') as file:
-            dump(items, file, pickle.HIGHEST_PROTOCOL)
-    except Exception as e:
-        print(e)
-        sys.exit()
+    with open(PICKLE_FILE, 'wb') as file:
+        dump(items, file, pickle.HIGHEST_PROTOCOL)
 
 
 def clone(repo_name: str) -> str:
+    """
+    Clones the given repo using the GitHub cli
+    :param repo_name: The name of the repo from GitHub
+    :return: The directory the repo was cloned to.
+    """
     print(REPOS_DIRECTORY)
     os.makedirs(REPOS_DIRECTORY, exist_ok=True)
     path = os.path.join(REPOS_DIRECTORY, repo_name.split('/')[-1])
     print(f'cloning repo {repo_name} to {path}')
-    subprocess.run(['gh', 'repo', 'clone', repo_name, path])
+    subprocess.run(['gh', 'repo', 'clone', repo_name, path], check=True)
     return path
 
 
 def push_repo(path: str):
+    """
+    Push the given repo.
+    :param path: The path to a repo.
+    :return: None
+    """
     print(f"committing and pushing {path}")
     date = datetime.now()
-    subprocess.run(['git', 'add', '.'], cwd=path)
-    subprocess.run(['git', 'commit', f'-m "{date}"'], cwd=path)
-    subprocess.run(['git', 'push'], cwd=path)
+    subprocess.run(['git', 'add', '.'], cwd=path, check=True)
+    subprocess.run(['git', 'commit', f'-m "{date}"'], cwd=path, check=True)
+    subprocess.run(['git', 'push'], cwd=path, check=True)
 
 
 def pull_repo(path: str):
+    """
+    Pull a given repo from GitHub
+    :param path: The path to the repo
+    :return: None
+    """
     print(f"pulling {path}")
-    subprocess.run(['git', 'pull'], cwd=path)
+    subprocess.run(['git', 'pull'], cwd=path, check=True)
 
 
 class RepoSearch(QRunnable):
@@ -105,21 +124,39 @@ class RepoSearch(QRunnable):
     """
 
     class Signals(QObject):
+        """
+        Simple signals to send in Qt
+        """
         result = pyqtSignal(str)
 
     signals = Signals()
 
     @pyqtSlot()
     def run(self):
+        """
+        Run the search for repos
+        :return:
+        """
         home = os.path.expanduser('~')
         repo_list = []
-        for root, dirs, files in os.walk(home):
+        for root, dirs, _ in os.walk(home):
             if '.git' in dirs:
                 repo_list.append(root)
                 self.signals.result.emit(root)
 
+    def stub(self):
+        """
+        This is only here to make pylint behave.
+        Evidently you can't have a class with only a single public method.
+        :return: None
+        """
+
 
 class RepoSelector(QDialog):
+    """
+    A popup dialog box to help select a repo to clone.
+    Uses the GitHub cli to generate a list of repos.
+    """
     buttonBox: QDialogButtonBox
     repoList: QListWidget
 
@@ -130,32 +167,48 @@ class RepoSelector(QDialog):
         repo_list: list[str] = subprocess.run(
             ['gh', 'repo', 'list'],
             capture_output=True,
+            check=True,
             encoding='utf-8').stdout.splitlines()
         for repo in repo_list:
             result: str = ''
             for char in repo:
                 if char == '\t':
                     break
-                else:
-                    result += char
+                result += char
             self.add_to_repo_list(result)
         self.repoList.currentItemChanged.connect(self.selection_changed)
 
     def add_to_repo_list(self, item: str):
+        """
+        Add a given item to the repo list if it isn't already there.
+        :param item: The item to add.
+        :return: None
+        """
         if len(self.repoList.findItems(item, Qt.MatchFlag.MatchExactly)) == 0:
             self.repoList.addItem(item)
 
     def selection_changed(self, new_selection: QListWidgetItem):
+        """
+        Saves the selected item as the result.
+        :param new_selection: The item just selected
+        :return: None
+        """
         self.result = new_selection.text()
 
     def accept(self):
+        """
+        Tests to see if the result has been selected before accepting it.
+        :return: None
+        """
         if self.result == '':
-            return False
-        else:
-            super().accept()
+            return
+        super().accept()
 
 
 class MainWindow(QMainWindow):
+    """
+    The Main Window of the program.
+    """
     pullAllButton: QPushButton
     pullRepoButton: QPushButton
     pushAllButton: QPushButton
@@ -193,12 +246,19 @@ class MainWindow(QMainWindow):
         self.cloneRepoButton.clicked.connect(self.clone_repo)
 
     def search_for_repos(self):
+        """
+        Search for repos in the users home folder and adds them to the listview
+        :return: None
+        """
         worker = RepoSearch()
         worker.signals.result.connect(self.add_to_repo_list)
         self.threadpool.start(worker)
 
     def clone_repo(self):
-        # TODO: show repo window and clone and add repo to repos directory.
+        """
+        Presents a dialog to select a repo to clone and add to the repo list
+        :return: None
+        """
         dialog = RepoSelector()
         result = dialog.exec()
         if result:
@@ -206,11 +266,20 @@ class MainWindow(QMainWindow):
             self.add_to_repo_list(path)
 
     def add_to_repo_list(self, item: str):
+        """
+        Add the given repo to the repo list if it isn't already there.
+        :param item: The repo to add.
+        :return: None
+        """
         if len(self.repoList.findItems(item, Qt.MatchFlag.MatchExactly)) == 0:
             self.repoList.addItem(item)
             save_repos(self.repoList)
 
     def manual_repo_add(self):
+        """
+        Presents a dialog to add a repo manually
+        :return: None
+        """
         dialog = QFileDialog(self)
         dialog.setFileMode(QFileDialog.FileMode.Directory)
         if dialog.exec():
@@ -219,30 +288,50 @@ class MainWindow(QMainWindow):
                 self.add_to_repo_list(os.path.abspath(directory))
 
     def rm_repo(self):
+        """
+        Removes the selected repo(s)
+        :return: None
+        """
         for item in self.repoList.selectedItems():
             row = self.repoList.row(item)
             self.repoList.takeItem(row)
         save_repos(self.repoList)
 
     def push_all(self):
+        """
+        Push all the repos!
+        :return: None
+        """
         repos = list_repos(self.repoList)
         for repo in repos:
             push_repo(repo)
         print('Finished Pushing all repos')
 
     def pull_all(self):
+        """
+        Pull all the repos!
+        :return: None
+        """
         repos = list_repos(self.repoList)
         for repo in repos:
             pull_repo(repo)
         print('Finished Pulling all repos')
 
     def push_selected(self):
+        """
+        Push the selected repos.
+        :return: None
+        """
         repos = self.repoList.selectedItems()
         for repo in repos:
             push_repo(repo.text())
         print('Finished pushing selected repos')
 
     def pull_selected(self):
+        """
+        Pull the selected repos.
+        :return: None
+        """
         repos = self.repoList.selectedItems()
         for repo in repos:
             pull_repo(repo.text())
