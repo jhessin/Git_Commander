90f792e2ca4e218a702f3bdecb270b2dab431ad3
Jim Hessin <jhessin@gmail.com>
2024-01-07 09:50:28 -0700

    Fresh briefcase generated project

---
+++ src/gitcommander/app.py
@@ -0,0 +1,27 @@
+"""
+A basic app for managing multiple git repos and pushing and pulling from github.
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class GitCommander(toga.App):
+
+    def startup(self):
+        """
+        Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return GitCommander()
