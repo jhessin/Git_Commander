91990b8771e03bee87462ff4ed8b09d3a1aa87e2
Jim Hessin <jhessin@gmail.com>
2024-11-25 07:27:26 -0700

    Switch to using Sizers

--- gitcommander/src/gitcommander/ExamplePanel.py
+++ gitcommander/src/gitcommander/ExamplePanel.py
@@ -4,23 +4,32 @@ import wx
 class ExamplePanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        self.quote = wx.StaticText(self, label="Your quote :", pos=(20, 30))
+
+        # Create some sizers
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        grid = wx.GridBagSizer(hgap=5, vgap=5)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Static Text
+        self.quote = wx.StaticText(self, label="Your quote :")
+        grid.Add(self.quote, pos=(0, 0))
 
         # A multiline TextCtrl - This is here to show how the events work in this program, don't pay too much attention to it
-
         self.logger = wx.TextCtrl(
-            self, pos=(300, 20), size=(200, 300), style=wx.TE_MULTILINE | wx.TE_READONLY
+            self, size=(200, 300), style=wx.TE_MULTILINE | wx.TE_READONLY
         )
 
         # A button
-        self.button = wx.Button(self, label="Save", pos=(200, 325))
+        self.button = wx.Button(self, label="Save")
         self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
 
         # the edit control - one line version
         self.lblname = wx.StaticText(self, label="Your name :", pos=(20, 60))
+        grid.Add(self.lblname, pos=(1, 0))
         self.editname = wx.TextCtrl(
             self, value="Enter here your name", pos=(150, 60), size=(140, -1)
         )
+        grid.Add(self.editname, pos=(1, 1))
         self.Bind(wx.EVT_TEXT, self.EvtText, self.editname)
         self.Bind(wx.EVT_CHAR, self.EvtChar, self.editname)
 
@@ -29,6 +38,7 @@ class ExamplePanel(wx.Panel):
         self.lblhear = wx.StaticText(
             self, label="How did you hear from us?", pos=(20, 90)
         )
+        grid.Add(self.lblhear, pos=(3, 0))
         self.edithear = wx.ComboBox(
             self,
             pos=(150, 90),
@@ -36,13 +46,18 @@ class ExamplePanel(wx.Panel):
             choices=self.sampleList,
             style=wx.CB_DROPDOWN,
         )
+        grid.Add(self.edithear, pos=(3, 1))
         self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.edithear)
         self.Bind(wx.EVT_TEXT, self.EvtText, self.edithear)
+
+        # add a spacer to the sizer
+        grid.Add((10, 40), pos=(2, 0))
 
         # Checkbox
         self.insure = wx.CheckBox(
             self, label="Do you want Insured Shipment?", pos=(20, 180)
         )
+        grid.Add(self.insure, pos=(4, 0), span=(1, 2), flag=wx.BOTTOM, border=5)
         self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.insure)
 
         # Radio Boxes
@@ -65,7 +80,14 @@ class ExamplePanel(wx.Panel):
             majorDimension=3,
             style=wx.RA_SPECIFY_COLS,
         )
+        grid.Add(rb, pos=(5, 0), span=(1, 2))
         self.Bind(wx.EVT_RADIOBOX, self.EvtRadioBox, rb)
+
+        hSizer.Add(grid, 0, wx.ALL, 5)
+        hSizer.Add(self.logger)
+        mainSizer.Add(hSizer, 0, wx.ALL, 5)
+        mainSizer.Add(self.button, 0, wx.CENTER)
+        self.SetSizerAndFit(mainSizer)
 
     def EvtRadioBox(self, event):
         self.logger.AppendText("EvtRadioBox: %d\n" % event.GetInt())
