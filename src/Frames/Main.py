c99be23e5e0a37e29010ed318eef6c675748a053
Jim Hessin <jhessin@gmail.com>
2025-08-25 21:08:14 -0600

    "2025-08-25 21:08:13.962754"

--- src/Frames/Main.py
+++ src/Frames/Main.py
@@ -1,67 +1,81 @@
 import os
 import wx
 
+from src.Panels.repo_list_view import RepoListView
+
 
 class Main(wx.Frame):
-    control: wx.TextCtrl
+    filename: str
+    dir_name: str
+    left_list: RepoListView
+    right_list: RepoListView
 
     def __init__(self, parent, title, *args, **kw):
         super().__init__(parent, title=title, size=(200, -1), *args, **kw)
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+
+        # Creating a text control for entering data.
+        # self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+
         self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
+        # Setting up the list views
+        self.left_list = RepoListView(self)
+        self.right_list = RepoListView(self)
+
         # Setting up the menu.
-        filemenu = wx.Menu()
+        # file_menu = wx.Menu()
         # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", "Open a file for editing")
-        menuAbout = filemenu.Append(
-            wx.ID_ABOUT, "&About", "Information about this program"
-        )
-        filemenu.AppendSeparator()
-        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
+        # menu_open = file_menu.Append(wx.ID_OPEN, "&Open", "Open a file for editing")
+        # menu_about = file_menu.Append(
+        #     wx.ID_ABOUT, "&About", "Information about this program"
+        # )
+        # file_menu.AppendSeparator()
+        # menu_exit = file_menu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
 
         # Creating the menubar
-        menuBar = wx.MenuBar()
-        menuBar.Append(filemenu, "&File")  # Adding the filemenu to the menubar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+        # menu_bar = wx.MenuBar()
+        # menu_bar.Append(file_menu, "&File")  # Adding the filemenu to the menubar
+        # self.SetMenuBar(menu_bar)  # Adding the MenuBar to the Frame content.
 
         # Set events
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        # self.Bind(wx.EVT_MENU, self.on_about, menu_about)
+        # self.Bind(wx.EVT_MENU, self.on_exit, menu_exit)
+        # self.Bind(wx.EVT_MENU, self.on_open, menu_open)
 
         # Set up sizers
-        self.horizontalSizer: wx.BoxSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.buttons = []
-        for i in range(0, 6):
-            self.buttons.append(wx.Button(self, -1, f"Button &{str(i)}"))
-            self.horizontalSizer.Add(self.buttons[i], 1, wx.EXPAND)
+        self.horizontal_sizer: wx.BoxSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.horizontal_sizer.Add(self.left_list, 1, wx.EXPAND)
+        self.horizontal_sizer.Add(self.right_list, 1, wx.EXPAND)
 
-        self.verticalSizer: wx.BoxSizer = wx.BoxSizer(wx.VERTICAL)
-        self.verticalSizer.Add(self.control, 1, wx.EXPAND)
-        self.verticalSizer.Add(self.horizontalSizer, 0, wx.EXPAND)
+        # self.buttons = []
+        # for i in range(0, 6):
+        #     self.buttons.append(wx.Button(self, -1, f"Button &{str(i)}"))
+        #     self.horizontal_sizer.Add(self.buttons[i], 1, wx.EXPAND)
+
+        self.vertical_sizer: wx.BoxSizer = wx.BoxSizer(wx.VERTICAL)
+        self.vertical_sizer.Add(self.horizontal_sizer, 0, wx.EXPAND)
 
         # layout sizers
-        self.SetSizer(self.verticalSizer)
+        self.SetSizer(self.vertical_sizer)
         self.SetAutoLayout(1)
-        self.verticalSizer.Fit(self)
+        self.vertical_sizer.Fit(self)
 
         # Show the window
         self.Show(True)
 
-    def OnOpen(self, e: wx.Event):
+    def on_open(self, e: wx.Event):
         """Open a file"""
-        self.dirname = ""
-        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.FD_OPEN)
+        self.dir_name = ""
+        dlg = wx.FileDialog(self, "Choose a file", self.dir_name, "", "*.*", wx.FD_OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
-            self.dirname = dlg.GetDirectory()
-            f = open(os.path.join(self.dirname, self.filename), "r")
+            self.dir_name = dlg.GetDirectory()
+            f = open(os.path.join(self.dir_name, self.filename), "r")
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
 
-    def OnAbout(self, event: wx.Event):
+    def on_about(self, event: wx.Event):
         # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets
         dlg = wx.MessageDialog(
             self, "A small text editor", "About Sample Editor", wx.OK
@@ -69,5 +83,5 @@ class Main(wx.Frame):
         dlg.ShowModal()  # Show it
         dlg.Destroy()  # finally destroy it when finished
 
-    def OnExit(self, event: wx.Event):
+    def on_exit(self, event: wx.Event):
         self.Close(True)
--- src/Panels/repo_list_view.py
+++ src/Panels/repo_list_view.py
@@ -3,12 +3,10 @@ This will handle the display and upkeep of the list of repositories.
 """
 
 import wx
-from Models.repo_list import RepoList
-
+from src.Models.repo_list import RepoList
 
 
 class RepoListView(wx.ListBox):
-
     data: RepoList = RepoList()
 
     def __init__(self, *args, **kwargs):
