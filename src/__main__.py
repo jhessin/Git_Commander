7e67cdc3b7a257755cef9f44a1b1cc9ce7e5bc55
Jim Hessin <jhessin@gmail.com>
2026-05-02 05:51:01 -0600

    20260502055101

--- src/__main__.py
+++ src/__main__.py
@@ -1,28 +1,6 @@
-import asyncio
+import sys
 
-import wx
-from wxasync import WxAsyncApp
+from .main import main
 
-from .MainFrame import MainFrame
-
-
-class GitCommander(WxAsyncApp):
-    frame: MainFrame
-
-    def OnInit(self):
-        self.frame = MainFrame(None, wx.ID_ANY, "")
-        self.SetTopWindow(self.frame)
-        self.frame.Show()
-        return True
-
-
-async def main_async():
-    app = GitCommander(1)
-    await app.MainLoop()
-
-def main():
-    asyncio.run(main_async())
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    main(sys.argv)
